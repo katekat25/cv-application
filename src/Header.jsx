44886a88a9daa9184751d9908e3b44d4997d0016
Kate Schumacher <kate.schumacher@gmail.com>
2025-05-14 12:49:38 -0500

    Correct misnamed classes in stylesheet

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
         <Icon className="icon" />
         <EditableInput type="text" value="Kate Schumacher" className="header-item name"/>
         <EditableInput multiline={true} value="Professional Summary" className="header-item summary"/>
-        <EditableInput type="text" value="katekat25.github.com" className="header-item"/>
-        <EditableInput type="email" value="kate.schumacher@gmail.com" className="header-item"/>
-        <EditableInput type="tel" value="(555) 555-5555" className="header-item"/>
-        <EditableInput type="text" value="123 Pleasant St., Anytown, USA" className="header-item"/>
+        <EditableInput type="text" value="katekat25.github.com" className="header-item website"/>
+        <EditableInput type="email" value="kate.schumacher@gmail.com" className="header-item email"/>
+        <EditableInput type="tel" value="(555) 555-5555" className="header-item number"/>
+        <EditableInput type="text" value="123 Pleasant St., Anytown, USA" className="header-item address"/>
     </div>
 }
 
